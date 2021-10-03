56e5a10f5d0102c08b44accbf1988bf7e1636fbc
Estefany Amado <estefany@Estefanys-MacBook-Pro.local>
2021-10-03 09:25:48 -0700

    Add changes made

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,23 +2,27 @@ import os
 import csv 
 
 #Your task is to create a Python script that analyzes the records to calculate each of the following:
-Budget_csv = os.path.join('..', 'Resources', 'budget_data.csv')
-Path = /Users/estefany/Documents/Python-Challenge/PyBank/Resources 
-with open(path) as csvfile:
+Bank_csv = os.path.join('PyBank',"Resources","budget_data.csv")
+
+Date_rows = 0
+Net_total = 0 
+
+with open(Bank_csv) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
-    print(csvreader)
+    
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
-    for row in csvreader:
-        print(row)
+
 
 #The total number of months included in the dataset
- 
-
+    for row in csvreader:
+      Date_rows = Date_rows + 1 
+    
 #The net total amount of "Profit/Losses" over the entire period
- 
+      Net_total = Net_total + int(row[1]) 
+    print ("Total : ", "$", (Net_total))
+    print ("Total Months : ", (Date_rows)) 
 #Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
 
 #The greatest increase in profits (date and amount) over the entire period
 
-#The greatest decrease in profits (date and amount) over the entire periodrr
+#The greatest decrease in profits (date and amount) over the entire period
