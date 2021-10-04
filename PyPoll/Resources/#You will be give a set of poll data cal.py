93b0dbd9492dc661490b363791fcb2dd88c14ec2
Estefany Amado <estefany@Estefanys-MacBook-Pro.local>
2021-10-03 21:49:22 -0700

    Import scripts to Pypoll

--- PyPoll/Resources/#You will be give a set of poll data cal.py
+++ PyPoll/Resources/#You will be give a set of poll data cal.py
@@ -4,8 +4,7 @@ import os
 import csv 
 
 Vote_count = 0
-Candidates = {}
-
+Candidates = {1: {'Name':'Khan', 'Votes': '2218231'}, 2: {'Name': 'Correy', 'Votes': '704200'}, 3: {'Name': 'Li', 'Votes': '492940'}, 4: {'Name': "O'Tooley", 'Votes': '105630'}} 
 
 Election_data_csv = os.path.join('PyPoll',"Resources","PyPoll_Resources_election_data.csv")
 
@@ -18,22 +17,16 @@ with open(Election_data_csv) as csvfile:
         Vote_count = Vote_count + 1 
 
 #A complete list of candidates who received votes
-        if row [2] in Candidates: 
-            Candidates[row[2]] += 1
-        else: 
-            Candidates[row[2]] = 1 
-
-K_percent = "{:.3%}".format(Candidates['Khan'] / Vote_count)
-C_percent = "{:.3%}".format(Candidates['Correy'] / Vote_count)
-L_percent = "{:.3%}".format(Candidates['Li'] / Vote_count)
-O_percent = "{:.3%}".format(Candidates["O'Tooley"] / Vote_count)
   
+
 print("Election Results")
 print("--------------------------")
 print("Total Votes:", Vote_count)
 print("--------------------------")
-print("Khan:", (Candidates['Khan']), K_percent)
-print("Correy:", (Candidates['Correy']), C_percent)
-print("Li:", (Candidates['Li']), L_percent) 
-print("O'Tooley:", (Candidates["O'Tooley"]), O_percent) 
-print("--------------------------")
+print(Candidates)
+
+#The percentage of votes each candidate won
+
+#The total number of votes each candidate won
+
+#The winner of the election based on popular vote.
