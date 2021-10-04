a1e2cd9dc9444bcea9216e1282aa23f42d800cff
Estefany Amado <estefany@Estefanys-MacBook-Pro.local>
2021-10-03 17:21:55 -0700

    Export file into text file

--- PyBank/main.py
+++ PyBank/main.py
@@ -28,15 +28,27 @@ with open(Bank_csv) as csvfile:
       for i in range(len(Profit_Losses)-1):
         Profit_Losses_change.append(Profit_Losses[i+1]-Profit_Losses[i])
         Months.append(row[0])
-        
+
 max_increase_value = max(Profit_Losses_change)
 max_increase_month = Profit_Losses_change.index(max(Profit_Losses_change)) 
 max_decrease_value = min(Profit_Losses_change)
 max_decrease_month = Profit_Losses_change.index(min(Profit_Losses_change))
  
-
+print ("Financial Analysis")
+print ("-----------------------------")
 print ("Total : ", "$", (Net_total))
 print ("Total Months : ", (Date_rows)) 
 print (f"Average Change: {round(sum(Profit_Losses_change)/len(Profit_Losses_change),2)}")
 print ("Greatest Increase in Profits: ", Months[max_increase_month], f"${(int(max_increase_value))}") 
 print ("Greatest Decrease in Profits: ", Months[max_decrease_month], f"${(int(max_decrease_value))}") 
+
+output_file = os.path.join("PyBank", "Analysis","Analysis.txt") 
+
+with open(output_file, "w") as file: 
+    file.write("Financial Analysis")
+    file.write('\n' +"-----------------------------")
+    file.write('\n' +"Total : $" + str(Net_total))
+    file.write('\n' +"Total Months :" + str(Date_rows)) 
+    file.write('\n' +f"Average Change: {round(sum(Profit_Losses_change)/len(Profit_Losses_change),2)}")
+    file.write('\n' +"Greatest Decrease in Profits: " + (Months[max_decrease_month] + (f" (${(str(max_decrease_value))})")))
+    file.write('\n' +"Greatest Increase in Profits: " + (Months[max_increase_month] + (f" (${(str(max_increase_value))})")))
